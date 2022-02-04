118ff6fd8f044769c8c9864160efbcbcb5be3d2e
swamiee <qw3a@naver.com>
2022-02-04 12:49:44 +0900

    .

--- 01/js/script.js
+++ 01/js/script.js
@@ -1,40 +1,32 @@
-$(".gnb").hover(
-    function(){ 
-        $(".sub").stop().slideDown();
-
-    },
+$(".tab li:eq(0)").click( //공지사항
     function(){
-        $(".sub").stop().slideUp();
+        // $("body").css("background","#ff0");\
+        $(".tab li a").removeClass("on")  ;
+        $(".tab li:eq(0) a").addClass("on") ;
+        $(".notice").show();
+        $(".gallery").hide(); 
+    
+       }
+);
+
+$(".tab li:eq(1)").click( //갤러리
+    function(){
+        // $("body").css("background","#fff");
+        $(".tab li a").removeClass("on")  ;
+        $(".tab li:eq(1) a").addClass("on")
+        $(".notice").hide();
+        $(".gallery").show();
         
     }
 );
 
-/* tap */
-
-$(".tap1").click( //공지사항 클릭하면
-    function(){
-        $(".notice").show();
-        $(".gallery").hide();
-    }
-
-);
-
-$(".tap2").click( //갤러리 클릭하면
-    function(){
-        $(".notice").hide();
-        $(".gallery").show();
-    }
-
-);
-
-/* 팝업 */
-$(".open").click( 
+$(".open").click(
     function(){
         $("#popup").fadeIn();
     }
 );
-$(".close").click( 
+$(".close").click(
     function(){
         $("#popup").fadeOut();
     }
-);
+)
