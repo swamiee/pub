867583a16360f2f4e0c6268df72b16baf21c8f29
swamiee <qw3a@naver.com>
2022-02-07 16:13:17 +0900

    .

--- 02/js/script.js
+++ 02/js/script.js
@@ -9,3 +9,14 @@ $(".gnb").hover(
     }
    
 );
+
+$(".open").click(
+    function(){
+        $("#popup").fadeIn();
+    }
+);
+$(".close").click(
+    function(){
+        $("#popup").fadeOut();
+    }
+);
